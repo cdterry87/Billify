a9c4ec39489ca5af66c3f6b65e45a851fb8f6e65
Chase Terry <chase.terry87@gmail.com>
2024-07-26 22:57:51 -0500

    Starting over

--- resources/views/layouts2/demo.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="col m12">
-    <article class="demo card-panel light-green white-text">
-        <div>
-            <p>To demo the application, use the following credentials:</p>
-            Username: admin@example.com <br>
-            Password: password
-        </div>
-    </article>
-</div>
