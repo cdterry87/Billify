baa57a31af2a210bc4b6973314cb5b517f8b732b
Chase Terry <chase.terry87@gmail.com>
2024-08-10 14:28:02 -0500

    Adds initial tests #20

---
+++ tests/Feature/ForgotPasswordTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ForgotPasswordTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
